65181e36a64031b34c54e9cd34623f1855f53f5a
Edward George <edwardgeorge@gmail.com>
2025-01-06 09:17:25 +0000

    createWindow immediately
    currently requires clicking on dock to create the window

--- desktop/main.js
+++ desktop/main.js
@@ -74,12 +74,12 @@ app.whenReady().then(() => {
     app.quit();
   });
 
-  app.on("activate", () => {
-    if (win === null) {
-      createWindow();
-    } else {
-      win.show();
-    }
+  createWindow();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
